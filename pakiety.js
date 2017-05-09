50e2b22d06622dcdacae508f3418619d9bb61e98
flaminka <ewa.baranowska4@wp.pl>
2017-05-09 10:11:58 +0200

    usunięcie podpisu  z infografiki + infografika.pdf

--- pakiety.js
+++ pakiety.js
@@ -40,6 +40,26 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "DBI",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/DBI",
+    "doswiadczenie_R_med": 1.25,
+    "doswiadczenie_analiza_med": 8.5,
+    "doswiadczenie_R_mean": 1.25,
+    "doswiadczenie_analiza_mean": 8.5,
+    "dosw": true
+  },
+  {
+    "pakiety": "devtools",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/devtools",
+    "doswiadczenie_R_med": 1.5,
+    "doswiadczenie_analiza_med": 1.5,
+    "doswiadczenie_R_mean": 1.5,
+    "doswiadczenie_analiza_mean": 1.5,
+    "dosw": false
+  },
+  {
     "pakiety": "dplyr",
     "Freq": 61,
     "url": "https://www.rdocumentation.org/packages/dplyr",
@@ -47,6 +67,56 @@ var danePakiety = [
     "doswiadczenie_analiza_med": 4,
     "doswiadczenie_R_mean": 2.9655,
     "doswiadczenie_analiza_mean": 4.5614,
+    "dosw": true
+  },
+  {
+    "pakiety": "DT",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/DT",
+    "doswiadczenie_R_med": 4.5,
+    "doswiadczenie_analiza_med": 3.5,
+    "doswiadczenie_R_mean": 4.5,
+    "doswiadczenie_analiza_mean": 3.5,
+    "dosw": false
+  },
+  {
+    "pakiety": "flexdashboard",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/flexdashboard",
+    "doswiadczenie_R_med": 2.5,
+    "doswiadczenie_analiza_med": 2.5,
+    "doswiadczenie_R_mean": 2.5,
+    "doswiadczenie_analiza_mean": 2.5,
+    "dosw": false
+  },
+  {
+    "pakiety": "forecast",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/forecast",
+    "doswiadczenie_R_med": 3,
+    "doswiadczenie_analiza_med": 4,
+    "doswiadczenie_R_mean": 3,
+    "doswiadczenie_analiza_mean": 4,
+    "dosw": true
+  },
+  {
+    "pakiety": "FSelectorRcpp",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/FSelectorRcpp",
+    "doswiadczenie_R_med": 4.5,
+    "doswiadczenie_analiza_med": 4,
+    "doswiadczenie_R_mean": 4.5,
+    "doswiadczenie_analiza_mean": 4,
+    "dosw": false
+  },
+  {
+    "pakiety": "gbm",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/gbm",
+    "doswiadczenie_R_med": 4.5,
+    "doswiadczenie_analiza_med": 8,
+    "doswiadczenie_R_mean": 4.5,
+    "doswiadczenie_analiza_mean": 8,
     "dosw": true
   },
   {
@@ -70,6 +140,36 @@ var danePakiety = [
     "dosw": false
   },
   {
+    "pakiety": "gmodels",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/gmodels",
+    "doswiadczenie_R_med": 1.5,
+    "doswiadczenie_analiza_med": 7,
+    "doswiadczenie_R_mean": 1.5,
+    "doswiadczenie_analiza_mean": 7,
+    "dosw": true
+  },
+  {
+    "pakiety": "googleAnalyticsR",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/googleAnalyticsR",
+    "doswiadczenie_R_med": 0.5,
+    "doswiadczenie_analiza_med": 3.5,
+    "doswiadczenie_R_mean": 0.5,
+    "doswiadczenie_analiza_mean": 3.5,
+    "dosw": true
+  },
+  {
+    "pakiety": "gtools",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/gtools",
+    "doswiadczenie_R_med": 1,
+    "doswiadczenie_analiza_med": 6,
+    "doswiadczenie_R_mean": 1,
+    "doswiadczenie_analiza_mean": 6,
+    "dosw": true
+  },
+  {
     "pakiety": "h2o",
     "Freq": 4,
     "url": "https://www.rdocumentation.org/packages/h2o",
@@ -88,6 +188,26 @@ var danePakiety = [
     "doswiadczenie_R_mean": 7,
     "doswiadczenie_analiza_mean": 8.5,
     "dosw": true
+  },
+  {
+    "pakiety": "highcharter",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/highcharter",
+    "doswiadczenie_R_med": 3,
+    "doswiadczenie_analiza_med": 3,
+    "doswiadczenie_R_mean": 3,
+    "doswiadczenie_analiza_mean": 3,
+    "dosw": false
+  },
+  {
+    "pakiety": "httr",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/httr",
+    "doswiadczenie_R_med": 5,
+    "doswiadczenie_analiza_med": 3.5,
+    "doswiadczenie_R_mean": 5,
+    "doswiadczenie_analiza_mean": 3.5,
+    "dosw": false
   },
   {
     "pakiety": "jsonlite",
@@ -120,6 +240,16 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "limma",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/limma",
+    "doswiadczenie_R_med": 6,
+    "doswiadczenie_analiza_med": 7,
+    "doswiadczenie_R_mean": 6,
+    "doswiadczenie_analiza_mean": 7,
+    "dosw": true
+  },
+  {
     "pakiety": "lme4",
     "Freq": 5,
     "url": "https://www.rdocumentation.org/packages/lme4",
@@ -127,6 +257,16 @@ var danePakiety = [
     "doswiadczenie_analiza_med": 4,
     "doswiadczenie_R_mean": 4.9,
     "doswiadczenie_analiza_mean": 5.4,
+    "dosw": true
+  },
+  {
+    "pakiety": "lmtest",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/lmtest",
+    "doswiadczenie_R_med": 2,
+    "doswiadczenie_analiza_med": 3.5,
+    "doswiadczenie_R_mean": 2,
+    "doswiadczenie_analiza_mean": 3.5,
     "dosw": true
   },
   {
@@ -140,6 +280,26 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "magrittr",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/magrittr",
+    "doswiadczenie_R_med": 5.5,
+    "doswiadczenie_analiza_med": 4,
+    "doswiadczenie_R_mean": 5.5,
+    "doswiadczenie_analiza_mean": 4,
+    "dosw": false
+  },
+  {
+    "pakiety": "maptools",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/maptools",
+    "doswiadczenie_R_med": 6,
+    "doswiadczenie_analiza_med": 9.5,
+    "doswiadczenie_R_mean": 6,
+    "doswiadczenie_analiza_mean": 9.5,
+    "dosw": true
+  },
+  {
     "pakiety": "multcomp",
     "Freq": 3,
     "url": "https://www.rdocumentation.org/packages/multcomp",
@@ -147,6 +307,26 @@ var danePakiety = [
     "doswiadczenie_analiza_med": 10,
     "doswiadczenie_R_mean": 3.1667,
     "doswiadczenie_analiza_mean": 11.3333,
+    "dosw": true
+  },
+  {
+    "pakiety": "nlme",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/nlme",
+    "doswiadczenie_R_med": 3.25,
+    "doswiadczenie_analiza_med": 4,
+    "doswiadczenie_R_mean": 3.25,
+    "doswiadczenie_analiza_mean": 4,
+    "dosw": true
+  },
+  {
+    "pakiety": "openxlsx",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/openxlsx",
+    "doswiadczenie_R_med": 7,
+    "doswiadczenie_analiza_med": 9,
+    "doswiadczenie_R_mean": 7,
+    "doswiadczenie_analiza_mean": 9,
     "dosw": true
   },
   {
@@ -160,6 +340,36 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "purrr",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/purrr",
+    "doswiadczenie_R_med": 8,
+    "doswiadczenie_analiza_med": 8,
+    "doswiadczenie_R_mean": 8,
+    "doswiadczenie_analiza_mean": 8,
+    "dosw": false
+  },
+  {
+    "pakiety": "randomForest",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/randomForest",
+    "doswiadczenie_R_med": 2.5,
+    "doswiadczenie_analiza_med": 2,
+    "doswiadczenie_R_mean": 2.5,
+    "doswiadczenie_analiza_mean": 2,
+    "dosw": false
+  },
+  {
+    "pakiety": "Rcmdr",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/Rcmdr",
+    "doswiadczenie_R_med": 8.5,
+    "doswiadczenie_analiza_med": 17,
+    "doswiadczenie_R_mean": 8.5,
+    "doswiadczenie_analiza_mean": 17,
+    "dosw": true
+  },
+  {
     "pakiety": "Rcpp",
     "Freq": 3,
     "url": "https://www.rdocumentation.org/packages/Rcpp",
@@ -167,6 +377,26 @@ var danePakiety = [
     "doswiadczenie_analiza_med": 11,
     "doswiadczenie_R_mean": 5.5,
     "doswiadczenie_analiza_mean": 10.75,
+    "dosw": true
+  },
+  {
+    "pakiety": "readr",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/readr",
+    "doswiadczenie_R_med": 1,
+    "doswiadczenie_analiza_med": 1.5,
+    "doswiadczenie_R_mean": 1,
+    "doswiadczenie_analiza_mean": 1.5,
+    "dosw": true
+  },
+  {
+    "pakiety": "relaimpo",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/relaimpo",
+    "doswiadczenie_R_med": 3,
+    "doswiadczenie_analiza_med": 5,
+    "doswiadczenie_R_mean": 3,
+    "doswiadczenie_analiza_mean": 5,
     "dosw": true
   },
   {
@@ -200,6 +430,26 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "ROracle",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/ROracle",
+    "doswiadczenie_R_med": 0.75,
+    "doswiadczenie_analiza_med": 9,
+    "doswiadczenie_R_mean": 0.75,
+    "doswiadczenie_analiza_mean": 9,
+    "dosw": true
+  },
+  {
+    "pakiety": "rpart",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/rpart",
+    "doswiadczenie_R_med": 1,
+    "doswiadczenie_analiza_med": 2,
+    "doswiadczenie_R_mean": 1,
+    "doswiadczenie_analiza_mean": 2,
+    "dosw": true
+  },
+  {
     "pakiety": "RPostgreSQL",
     "Freq": 3,
     "url": "https://www.rdocumentation.org/packages/RPostgreSQL",
@@ -210,6 +460,26 @@ var danePakiety = [
     "dosw": true
   },
   {
+    "pakiety": "RSelenium",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/RSelenium",
+    "doswiadczenie_R_med": 2,
+    "doswiadczenie_analiza_med": 3,
+    "doswiadczenie_R_mean": 2,
+    "doswiadczenie_analiza_mean": 3,
+    "dosw": true
+  },
+  {
+    "pakiety": "RSQLite",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/RSQLite",
+    "doswiadczenie_R_med": 2.5,
+    "doswiadczenie_analiza_med": 2.5,
+    "doswiadczenie_R_mean": 2.5,
+    "doswiadczenie_analiza_mean": 2.5,
+    "dosw": false
+  },
+  {
     "pakiety": "rvest",
     "Freq": 3,
     "url": "https://www.rdocumentation.org/packages/rvest",
@@ -218,6 +488,16 @@ var danePakiety = [
     "doswiadczenie_R_mean": 3.6667,
     "doswiadczenie_analiza_mean": 3.6667,
     "dosw": false
+  },
+  {
+    "pakiety": "sandwich",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/sandwich",
+    "doswiadczenie_R_med": 6,
+    "doswiadczenie_analiza_med": 7,
+    "doswiadczenie_R_mean": 6,
+    "doswiadczenie_analiza_mean": 7,
+    "dosw": true
   },
   {
     "pakiety": "shiny",
@@ -237,6 +517,16 @@ var danePakiety = [
     "doswiadczenie_analiza_med": 15,
     "doswiadczenie_R_mean": 7,
     "doswiadczenie_analiza_mean": 12.3333,
+    "dosw": true
+  },
+  {
+    "pakiety": "sqldf",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/sqldf",
+    "doswiadczenie_R_med": 1,
+    "doswiadczenie_analiza_med": 3,
+    "doswiadczenie_R_mean": 1,
+    "doswiadczenie_analiza_mean": 3,
     "dosw": true
   },
   {
@@ -290,6 +580,16 @@ var danePakiety = [
     "dosw": false
   },
   {
+    "pakiety": "tableone",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/tableone",
+    "doswiadczenie_R_med": 5,
+    "doswiadczenie_analiza_med": 12.5,
+    "doswiadczenie_R_mean": 5,
+    "doswiadczenie_analiza_mean": 12.5,
+    "dosw": true
+  },
+  {
     "pakiety": "tidyr",
     "Freq": 18,
     "url": "https://www.rdocumentation.org/packages/tidyr",
@@ -340,6 +640,16 @@ var danePakiety = [
     "dosw": false
   },
   {
+    "pakiety": "xlsx",
+    "Freq": 2,
+    "url": "https://www.rdocumentation.org/packages/xlsx",
+    "doswiadczenie_R_med": 4,
+    "doswiadczenie_analiza_med": 3.5,
+    "doswiadczenie_R_mean": 4.25,
+    "doswiadczenie_analiza_mean": 5.875,
+    "dosw": true
+  },
+  {
     "pakiety": "xts",
     "Freq": 3,
     "url": "https://www.rdocumentation.org/packages/xts",
@@ -349,4 +659,5 @@ var danePakiety = [
     "doswiadczenie_analiza_mean": 9.6667,
     "dosw": true
   }
-];
+] 
+;
